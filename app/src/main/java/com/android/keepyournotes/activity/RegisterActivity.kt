3f955d3da4fa1ef608f4edf11876109094dac78c
heelbui <huuhieu992001@gmail.com>
2022-07-25 17:28:19 +0700

    Add: check email while register

--- app/src/main/java/com/android/keepyournotes/activity/RegisterActivity.kt
+++ app/src/main/java/com/android/keepyournotes/activity/RegisterActivity.kt
@@ -58,7 +58,19 @@ class RegisterActivity : AppCompatActivity() {
                     binding.tvEmailError.text = getText(R.string.please_enter_your_email)
                 } else {
                     if (isValidEmail(binding.etEmail.text.toString())) {
-                        binding.tvEmailError.visibility = View.GONE
+                        fAuth.fetchSignInMethodsForEmail(binding.etEmail.text.toString())
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    if (task.result.signInMethods.isNullOrEmpty()) {
+                                        binding.tvEmailError.visibility = View.GONE
+                                    } else {
+                                        binding.tvEmailError.visibility = View.VISIBLE
+                                        binding.tvEmailError.text = getText(R.string.email_exists)
+                                    }
+                                } else {
+                                    Log.e("TAG", "fetchSignInMethodsForEmail: failed")
+                                }
+                            }
                     } else {
                         binding.tvEmailError.visibility = View.VISIBLE
                         binding.tvEmailError.text = getText(R.string.wrong_email_format)
